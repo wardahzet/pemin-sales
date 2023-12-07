edede1e4d7910a8570b0e484ece69cfb53b683f4
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-12-07 20:18:19 +0700

    punya wejed

--- src/pages/Sales/ShowSales.jsx
+++ src/pages/Sales/ShowSales.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { getSalesData } from "../../services/Sales";
+import { Link } from "react-router-dom";
+import { Dropdown } from 'flowbite-react';
 
 export default function ShowSales() {
-  const [saleData, setSalesData] = useState([]);  
-  const [itahun, setTahun] = useState(0);
-  const [ibulan, setBulan] = useState(0);
-  const [iproduct, setProduct] = useState(0);
-  
+  const [saleData, setSalesData] = useState([]); 
   useEffect(() => {
     document.title = "Sales | MegaMinyak Energi";
     getSalesData()
@@ -61,9 +59,11 @@ export default function ShowSales() {
       <div className="p-10 min-h-screen w-full">
         <div className="bg-[#6A93FF] p-5 items-center h-[86px] justify-between flex rounded-t-3xl">
           <h1 className="text-3xl font-semibold text-white">PENJUALAN</h1>
-          <button className="bg-[#FFD56A] w-[140px] text-black px-4 py-2 text-sm font-semibold">
-            Tambah
-          </button>
+          <Link to='/sales/add'>
+            <button className="bg-[#FFD56A] w-[140px] text-black px-4 py-2 text-sm font-semibold">
+              Tambah
+            </button>
+          </Link>
         </div>
         <div className="bg-white rounded-b-3xl border-2 border-[#6A93FF] mt-5 overflow-auto max-h-100">
           <table className="w-full">
